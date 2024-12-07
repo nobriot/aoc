fea947e187a5d3cfb3916183916b10c4b3933cb8
Nicolas Woltmann <github@nico-ow.mozmail.com>
2024-12-07 17:27:35 +0100

    Added day 7 ... Jumped a bit ahead

--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -2,11 +2,13 @@ use aoc2024::day1;
 use aoc2024::day2;
 use aoc2024::day3;
 use aoc2024::day4;
+use aoc2024::day7;
 
 const DAY_1_INPUT: &str = include_str!("../input/day1.txt");
 const DAY_2_INPUT: &str = include_str!("../input/day2.txt");
 const DAY_3_INPUT: &str = include_str!("../input/day3.txt");
 const DAY_4_INPUT: &str = include_str!("../input/day4.txt");
+const DAY_7_INPUT: &str = include_str!("../input/day7.txt");
 
 fn main() {
     // Day 1
@@ -24,4 +26,8 @@ fn main() {
     // Day 4
     println!("Solving Day 4.");
     day4::solve(DAY_4_INPUT);
+
+    // Day 7
+    println!("Solving Day 7.");
+    day7::solve(DAY_7_INPUT);
 }
