a1d7559e78557a7c7d828ee9c108629c3da9ba28
Nicolas Woltmann <github@nico-ow.mozmail.com>
2024-12-10 13:37:37 +0100

    Restructured more - gave up on trying macros

--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -1,13 +1,29 @@
 use clap::Parser;
 mod days;
 
-macro_rules! solve_day {
-    ($nb:ident) => {
+macro_rules! solve_day_inner {
+    ($nb:literal, $mod:ident, $args:ident, $input:ident) => {
         // Day $nb
-        if args.all || args.days.contains(&$nb) {
-            println!("Solving Day $nb.");
-            days::day01::solve(DAY_1_INPUT);
+        if $args.all || $args.days.contains(&$nb) {
+            println!("Solving Day {}.", $nb);
+            days::$mod::solve($input);
         }
+    };
+}
+
+macro_rules! solve_day {
+    ($nb:literal, $args:ident) => {
+        solve_day!(
+            $nb,
+            concat_idents!(day, $nb),
+            args,
+            concat_idents!(DAY_, $nb, _INPUT)
+        )
+    };
+    ($nb:literal, $mod:expr, $args:tt, $input:ident) => {
+        assert!($nb <= 24, "days stop at 24");
+        assert!($nb > 0, "days start at 1");
+        solve_day_inner!($nb, $mod, $args, $input)
     };
 }
 
@@ -47,68 +63,126 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    println!("Days : {:?}", args.days);
-    days::day01::solve("");
 
-    //solve_day!(1);
-
-    //     // Day 1
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 1.");
-    //         aoc2024::days::day01::solve(DAY_1_INPUT);
-    //     }
-    //
-    //     // Day 2
-    //     if args.all || args.days.contains(&2) {
-    //         println!("Solving Day 2.");
-    //         day2::solve(DAY_2_INPUT);
-    //     }
-    //
-    //     // Day 3
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 3.");
-    //         day3::solve(DAY_3_INPUT);
-    //     }
-    //
-    //     // Day 4
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 4.");
-    //         day4::solve(DAY_4_INPUT);
-    //     }
-    //
-    //     // Day 5
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 5.");
-    //         day5::solve(DAY_5_INPUT);
-    //     }
-    //
-    //     // Day 6
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 6.");
-    //         day6::solve(DAY_6_INPUT);
-    //     }
-    //
-    //     // Day 7
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 7.");
-    //         day7::solve(DAY_7_INPUT);
-    //     }
-    //
-    //     // Day 8
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 8.");
-    //         day8::solve(DAY_8_INPUT);
-    //     }
-    //
-    //     // Day 9
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 9.");
-    //         day9::solve(DAY_9_INPUT);
-    //     }
-    //
-    //     // Day 10
-    //     if args.all || args.days.contains(&1) {
-    //         println!("Solving Day 10.");
-    //         day10::solve(DAY_10_INPUT);
-    //     }
+    // solve_day!(1, args);
+    // Day 1
+    if args.all || args.days.contains(&1) {
+        println!("Solving Day {}.", 1);
+        days::day01::solve(DAY_1_INPUT);
+    }
+    // Day 2
+    if args.all || args.days.contains(&2) {
+        println!("Solving Day {}.", 2);
+        days::day02::solve(DAY_2_INPUT);
+    }
+    // Day 3
+    if args.all || args.days.contains(&3) {
+        println!("Solving Day {}.", 3);
+        days::day03::solve(DAY_3_INPUT);
+    }
+    // Day 4
+    if args.all || args.days.contains(&4) {
+        println!("Solving Day {}.", 4);
+        days::day04::solve(DAY_4_INPUT);
+    }
+    // Day 5
+    if args.all || args.days.contains(&5) {
+        println!("Solving Day {}.", 5);
+        days::day05::solve(DAY_5_INPUT);
+    }
+    // Day 6
+    if args.all || args.days.contains(&6) {
+        println!("Solving Day {}.", 6);
+        days::day06::solve(DAY_6_INPUT);
+    }
+    // Day 7
+    if args.all || args.days.contains(&7) {
+        println!("Solving Day {}.", 7);
+        days::day07::solve(DAY_7_INPUT);
+    }
+    // Day 8
+    if args.all || args.days.contains(&8) {
+        println!("Solving Day {}.", 8);
+        days::day08::solve(DAY_8_INPUT);
+    }
+    // Day 9
+    if args.all || args.days.contains(&9) {
+        println!("Solving Day {}.", 9);
+        days::day09::solve(DAY_9_INPUT);
+    }
+    // Day 10
+    if args.all || args.days.contains(&10) {
+        println!("Solving Day {}.", 10);
+        days::day10::solve(DAY_10_INPUT);
+    }
+    // Day 11
+    if args.all || args.days.contains(&11) {
+        println!("Solving Day {}.", 11);
+        days::day11::solve(DAY_11_INPUT);
+    }
+    // Day 12
+    if args.all || args.days.contains(&12) {
+        println!("Solving Day {}.", 12);
+        days::day12::solve(DAY_12_INPUT);
+    }
+    // Day 13
+    if args.all || args.days.contains(&13) {
+        println!("Solving Day {}.", 13);
+        days::day13::solve(DAY_13_INPUT);
+    }
+    // Day 14
+    if args.all || args.days.contains(&14) {
+        println!("Solving Day {}.", 14);
+        days::day14::solve(DAY_14_INPUT);
+    }
+    // Day 15
+    if args.all || args.days.contains(&15) {
+        println!("Solving Day {}.", 15);
+        days::day15::solve(DAY_15_INPUT);
+    }
+    // Day 16
+    if args.all || args.days.contains(&16) {
+        println!("Solving Day {}.", 16);
+        days::day16::solve(DAY_16_INPUT);
+    }
+    // Day 17
+    if args.all || args.days.contains(&17) {
+        println!("Solving Day {}.", 17);
+        days::day17::solve(DAY_17_INPUT);
+    }
+    // Day 18
+    if args.all || args.days.contains(&18) {
+        println!("Solving Day {}.", 18);
+        days::day18::solve(DAY_18_INPUT);
+    }
+    // Day 19
+    if args.all || args.days.contains(&19) {
+        println!("Solving Day {}.", 19);
+        days::day19::solve(DAY_19_INPUT);
+    }
+    // Day 20
+    if args.all || args.days.contains(&20) {
+        println!("Solving Day {}.", 20);
+        days::day20::solve(DAY_20_INPUT);
+    }
+    // Day 21
+    if args.all || args.days.contains(&21) {
+        println!("Solving Day {}.", 21);
+        days::day21::solve(DAY_21_INPUT);
+    }
+    // Day 22
+    if args.all || args.days.contains(&22) {
+        println!("Solving Day {}.", 22);
+        days::day22::solve(DAY_22_INPUT);
+    }
+    // Day 23
+    if args.all || args.days.contains(&23) {
+        println!("Solving Day {}.", 23);
+        days::day23::solve(DAY_23_INPUT);
+    }
+    // Day 24
+    if args.all || args.days.contains(&24) {
+        println!("Solving Day {}.", 24);
+        days::day24::solve(DAY_24_INPUT);
+    }
 }
