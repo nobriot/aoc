9403a202c890078ce1e7b8c3c2e5a2dcead37143
Nicolas Woltmann <github@nico-ow.mozmail.com>
2024-12-31 07:34:21 +0100

    Failed attempt at 21 part 2
    It needs to be optimized

--- aoc2024/src/days/day21.rs
+++ aoc2024/src/days/day21.rs
@@ -1,14 +1,12 @@
 use crate::input;
 use itertools::Itertools;
-use std::collections::HashMap;
-use std::collections::HashSet;
 
 pub fn solve() -> (Option<usize>, Option<usize>) {
     let input = input::DAY_21_INPUT;
     let codes = Codes::from_str(input);
 
     let part_1_total = solve_part_1(&codes);
-    let part_2_total = solve_part_2();
+    let part_2_total = solve_part_2(&codes);
 
     (part_1_total, part_2_total)
 }
@@ -17,8 +15,9 @@ fn solve_part_1(codes: &Codes) -> Option<usize> {
     Some(codes.solve())
 }
 
-fn solve_part_2() -> Option<usize> {
+fn solve_part_2(codes: &Codes) -> Option<usize> {
     None
+    //Some(codes.solve_part_2())
 }
 
 #[derive(Debug)]
@@ -61,6 +60,19 @@ impl<'a> Codes<'a> {
         None
     }
 
+    pub fn keep_shortest(codes: &mut Vec<String>) {
+        let mut shortest = 0;
+        let mut shortest_length = codes[0].len();
+        codes.iter().enumerate().for_each(|(i, a)| {
+            if a.len() < shortest_length {
+                shortest = i;
+                shortest_length = a.len();
+            }
+        });
+
+        codes.retain(|c| c.len() == shortest_length);
+    }
+
     pub fn solve(&self) -> usize {
         let mut total = 0;
         for code in &self.codes {
@@ -68,33 +80,14 @@ impl<'a> Codes<'a> {
             println!("Solving Code: {}", code);
             let first_codes = Self::solve_keypad(&Self::NUM_KEYPAD, *code);
 
-            // println!("First solutions:");
-            // println!("{:?}", first_codes);
-            let mut second_codes: Vec<String> = Vec::new();
-            first_codes.iter().for_each(|c| {
-                second_codes.append(&mut Self::solve_keypad(&Self::ARROW_KEYPAD, c));
-            });
-
-            // println!("Second solutions:");
-            // println!("{:?}", second_codes);
-            // panic!();
-            let mut third_codes: Vec<String> = Vec::new();
-            second_codes.iter().for_each(|c| {
-                third_codes.append(&mut Self::solve_keypad(&Self::ARROW_KEYPAD, c));
-            });
-
-            // Select the shortest of the thrid codes:
-            let mut shortest = 0;
-            let mut shortest_length = third_codes[0].len();
-            third_codes.iter().enumerate().for_each(|(i, a)| {
-                if a.len() < shortest_length {
-                    shortest = i;
-                    shortest_length = a.len();
-                }
-            });
+            let mut final_codes = Self::iterate_keypad(&Self::ARROW_KEYPAD, first_codes, 2);
 
             // Update the total
-            let solution = third_codes.swap_remove(shortest);
+            for s in &final_codes {
+                // println!("Solution: {}", s);
+            }
+            println!("number of solutions : {}", final_codes.len());
+            let solution = final_codes.swap_remove(0);
             let mut code_num = String::from(*code);
             code_num = code_num.replace("A", "");
             let code_num = code_num.parse::<usize>().unwrap();
@@ -106,82 +99,47 @@ impl<'a> Codes<'a> {
         total
     }
 
-    pub fn generate_paths(
-        keypad: &[&[char]],
-        from: (isize, isize),
-        to: (isize, isize),
-    ) -> Vec<Vec<char>> {
-        let position = from;
-        let target = to;
+    pub fn solve_part_2(&self) -> usize {
+        let mut total = 0;
+        for code in &self.codes {
+            println!("-----------------------------------");
+            println!("Solving Code: {}", code);
+            let first_codes = Self::solve_keypad(&Self::NUM_KEYPAD, *code);
 
-        let mut items: Vec<char> = Vec::new();
-        if target.1 > position.1 {
-            for _ in 0..(target.1 - position.1) as usize {
-                items.push('>');
-            }
-        }
-        if target.0 < position.0 {
-            for _ in 0..(position.0 - target.0) as usize {
-                items.push('^');
-            }
-        }
-        if target.0 > position.0 {
-            for _ in 0..(target.0 - position.0) as usize {
-                items.push('v');
-            }
-        }
-        if target.1 < position.1 {
-            for _ in 0..(position.1 - target.1) as usize {
-                items.push('<');
-            }
+            let mut final_codes = Self::iterate_keypad(&Self::ARROW_KEYPAD, first_codes, 25);
+
+            // Update the total
+            println!("number of solutions : {}", final_codes.len());
+            let solution = final_codes.swap_remove(0);
+            let mut code_num = String::from(*code);
+            code_num = code_num.replace("A", "");
+            let code_num = code_num.parse::<usize>().unwrap();
+            println!("Solution: {} - {}", code_num, solution.len());
+
+            total += code_num * solution.len();
         }
 
-        let k = items.len();
-        let paths = items.into_iter().permutations(k);
-        let mut valid_paths = Vec::new();
+        total
+    }
 
-        //println!("candidate Paths: {:?}", paths);
-        'p_loop: for path in paths {
-            let mut c_pos = from;
-            for mv in &path {
-                match mv {
-                    'v' => {
-                        c_pos.0 += 1;
-                        let target = Self::at(keypad, c_pos.0, c_pos.1);
-                        if target.is_none() || target.unwrap() == ' ' {
-                            continue 'p_loop;
-                        }
-                    }
-                    '^' => {
-                        c_pos.0 -= 1;
-                        let target = Self::at(keypad, c_pos.0, c_pos.1);
-                        if target.is_none() || target.unwrap() == ' ' {
-                            continue 'p_loop;
-                        }
-                    }
-                    '<' => {
-                        c_pos.1 -= 1;
-                        let target = Self::at(keypad, c_pos.0, c_pos.1);
-                        if target.is_none() || target.unwrap() == ' ' {
-                            continue 'p_loop;
-                        }
-                    }
-                    '>' => {
-                        c_pos.1 += 1;
-                        let target = Self::at(keypad, c_pos.0, c_pos.1);
-                        if target.is_none() || target.unwrap() == ' ' {
-                            continue 'p_loop;
-                        }
-                    }
-                    _ => panic!("Unexpected char"),
-                }
-            }
-            valid_paths.push(path);
+    pub fn iterate_keypad(keypad: &[&[char]], codes: Vec<String>, i: usize) -> Vec<String> {
+        println!(
+            "Iterating keypad: {} - size of the codes {}",
+            i,
+            codes.len()
+        );
+
+        if i == 0 {
+            return codes;
         }
-
-        valid_paths.sort();
-        valid_paths.dedup();
-        valid_paths
+        let mut new_codes: Vec<String> = Vec::new();
+        codes.iter().for_each(|c| {
+            new_codes.append(&mut Self::solve_keypad(keypad, c));
+        });
+        Self::keep_shortest(&mut new_codes);
+        new_codes.drain(1..);
+        new_codes = Self::iterate_keypad(keypad, new_codes, i - 1);
+        new_codes
     }
 
     pub fn solve_keypad(keypad: &[&[char]], keys: &str) -> Vec<String> {
@@ -189,11 +147,11 @@ impl<'a> Codes<'a> {
         let mut combinations: Vec<String> = Vec::new();
 
         for key in keys.chars() {
-            // println!("key: {}", key);
+            //println!("key: {}", key);
             let target = Self::find_char(keypad, key).unwrap();
-            //println!("target: {:?}", target);
+            // println!("target: {:?}", target);
             let paths = Self::generate_paths(keypad, position, target);
-            //println!("paths: {:?}", paths);
+            // println!("paths: {:?}", paths);
             let mut new_combinations = Vec::new();
 
             if combinations.is_empty() {
@@ -217,5 +175,79 @@ impl<'a> Codes<'a> {
         }
 
         combinations
+    }
+
+    pub fn generate_paths(
+        keypad: &[&[char]],
+        from: (isize, isize),
+        to: (isize, isize),
+    ) -> Vec<Vec<char>> {
+        let position = from;
+        let target = to;
+
+        let mut items: Vec<String> = Vec::new();
+        if target.0 < position.0 {
+            items.push("^".repeat((position.0 - target.0) as usize));
+        }
+        if target.1 > position.1 {
+            items.push(">".repeat((target.1 - position.1) as usize));
+        }
+        if target.0 > position.0 {
+            items.push("v".repeat((target.0 - position.0) as usize));
+        }
+        if target.1 < position.1 {
+            items.push("<".repeat((position.1 - target.1) as usize));
+        }
+
+        let k = items.len();
+        let paths = items.into_iter().permutations(k);
+        let mut valid_paths = Vec::new();
+
+        // println!("candidate Paths: {:?}", paths);
+        'p_loop: for path in paths {
+            let mut c_pos = from;
+            for moves in &path {
+                for mv in moves.chars() {
+                    match mv {
+                        'v' => {
+                            c_pos.0 += 1;
+                            let target = Self::at(keypad, c_pos.0, c_pos.1);
+                            if target.is_none() || target.unwrap() == ' ' {
+                                continue 'p_loop;
+                            }
+                        }
+                        '^' => {
+                            c_pos.0 -= 1;
+                            let target = Self::at(keypad, c_pos.0, c_pos.1);
+                            if target.is_none() || target.unwrap() == ' ' {
+                                continue 'p_loop;
+                            }
+                        }
+                        '<' => {
+                            c_pos.1 -= 1;
+                            let target = Self::at(keypad, c_pos.0, c_pos.1);
+                            if target.is_none() || target.unwrap() == ' ' {
+                                continue 'p_loop;
+                            }
+                        }
+                        '>' => {
+                            c_pos.1 += 1;
+                            let target = Self::at(keypad, c_pos.0, c_pos.1);
+                            if target.is_none() || target.unwrap() == ' ' {
+                                continue 'p_loop;
+                            }
+                        }
+                        _ => panic!("Unexpected char"),
+                    }
+                }
+            }
+            let mut char_path = Vec::new();
+            path.iter()
+                .for_each(|s| char_path.append(&mut s.chars().collect::<Vec<char>>()));
+            valid_paths.push(char_path);
+        }
+        //valid_paths.sort();
+        //valid_paths.dedup();
+        valid_paths
     }
 }
